00326cf989c3304863b846bc37d459cede85061b
LucasAliasElvennope <lucasmoreaupro0@gmail.com>
2025-05-23 09:28:36 +0200

    feat : Finished attack function

--- js/monster_slayer.js
+++ js/monster_slayer.js
@@ -8,25 +8,28 @@ const attaqueMax = 20;
 const attaqueSpéciale = 25;
 const attaqueSpécialMax = 50;
 const soin = 20;
-const abandon = JeuLancé = false;
 
 function abandonner() {
     if (!JeuLancé) return;
     JeuLancé = false;
     alert("Tu as abandonné le combat !");
 }
-  
+
 function genererNombreAleatoire(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function attaquer(){
     if (JeuLancé === true){
-
+    const dégats = genererNombreAleatoire(attaqueMin, attaqueMax); 
+    VieMonstre = VieMonstre - dégats;
 }
 if (JeuLancé === false){
     alert("Le jeu est terminé")
     return
+}
+if (VieMonstre < 0){
+    VieMonstre = 0;
 }
 }
 
