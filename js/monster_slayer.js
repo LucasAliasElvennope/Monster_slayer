726953d9646bffff96297336249f597b23f02abb
LucasAliasElvennope <lucasmoreaupro0@gmail.com>
2025-05-23 09:14:45 +0200

    feat : add the const for the give up

--- js/monster_slayer.js
+++ js/monster_slayer.js
@@ -8,11 +8,14 @@ const attaqueMax = 20;
 const attaqueSpéciale = 25;
 const attaqueSpécialMax = 50;
 const soin = 20;
+const abandon = JeuLancé = false;
+
 
 function genererNombreAleatoire(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-    
 
+function attaquer(){
+}
     
