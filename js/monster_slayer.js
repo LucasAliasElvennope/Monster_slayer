3f6dde9f1cd6502767822cb9a84c5bd2b063818b
LucasAliasElvennope <lucasmoreaupro0@gmail.com>
2025-05-23 09:11:47 +0200

    feat : adding the function to have a random number

--- js/monster_slayer.js
+++ js/monster_slayer.js
@@ -9,9 +9,10 @@ const attaqueSpéciale = 25;
 const attaqueSpécialMax = 50;
 const soin = 20;
 
+function genererNombreAleatoire(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-
-
-
+    
 
     
